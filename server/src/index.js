58162519d19799c3fd8e0a2ceb8b309ccba614d7
Mohammad Al Rafi <arafi304@gmail.com>
2025-03-08 18:33:30 +0600

    feat(backend): add basic cors (allow all)
    All origins are allowed now for development purposes

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import * as db from './db.js';
 import * as env from './env.js';
 
 import authRoutes from './routes/auth.js';
 
 const server = express();
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}
 
 const start = async () => {
     
     // Middlewares
+    server.use(cors(corsOptions));
     server.use(express.json());
 
     // Routes

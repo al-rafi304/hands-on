10ea74ad697800d99fa442c037607b2ac6f84542
Mohammad Al Rafi <arafi304@gmail.com>
2025-03-10 23:44:51 +0600

    feat(ui): empty frontend initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+	return (
+		<>
+			<div>
+				This will be the frontend for the hands on project
+			</div>
+		</>
+	)
+}
+
+export default App

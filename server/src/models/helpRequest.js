d96737a56fde176029426f0daeb3dc2ae22a0950
Mohammad Al Rafi <arafi304@gmail.com>
2025-03-15 19:35:35 +0600

    feat(help-req): define help request & comment schema models

---
+++ server/src/models/helpRequest.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import * as constants from '../constants.js';
+
+export const HelpRequestSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comments: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Comment',
+        default: []
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 200
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 1000
+    },
+    location: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: constants.CATEGORY,
+    },
+    is_open: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true });
+
+export default mongoose.model('HelpRequest', HelpRequestSchema);

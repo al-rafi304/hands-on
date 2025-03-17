c30b6c7b18a2a996d852ea627337b1313d8febcf
Mohammad Al Rafi <arafi304@gmail.com>
2025-03-17 23:53:06 +0600

    feat(comment): add likes field to for getComment using aggregators

--- server/src/controllers/comment.js
+++ server/src/controllers/comment.js
@@ -2,6 +2,7 @@ import User from '../models/user.js';
 import HelpRequest from '../models/helpRequest.js';
 import Comment from '../models/comment.js';
 import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
 
 export const createComment = async (req, res) => {
     const helpRequestId = req.params.helpRequestId;
@@ -32,7 +33,57 @@ export const createComment = async (req, res) => {
 export const getComments = async (req, res) => {
     const helpRequestId = req.params.helpRequestId;
 
-    const comments = await Comment.find({ helpRequest: helpRequestId }).populate('user', '_id name').sort({ createdAt: -1 });
+    // const comments = await Comment.find({ helpRequest: helpRequestId }).populate('user', '_id name').sort({ createdAt: -1 });
+
+    const comments = await Comment.aggregate([
+        { $match: { helpRequest: new mongoose.Types.ObjectId(String(helpRequestId)) } },
+        { $sort: { createdAt: 1 } },
+        
+        // Joining Likes and Comment 
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "comment",
+                as: "likes"
+            }
+        },
+
+        // Summing the 'likes' array and overwriting it with the count
+        {
+            $addFields: {
+                likes: { $size: "$likes" }
+            }
+        },
+
+        // Joining Users and Comment
+        // N.B: user field will contain an array
+        {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+
+                // Only selecting '_id', and 'name' field
+                pipeline: [
+                    {
+                        $project: {
+                            "_id": 1,
+                            "name": 1
+                        }
+                    }
+                ]
+            }
+        },
+
+        // Deconstructing the 'user' field array
+        {
+            $unwind: "$user"
+        }
+    ])
+
+
     if (!comments) {
         return res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid Help Request" })
     }
